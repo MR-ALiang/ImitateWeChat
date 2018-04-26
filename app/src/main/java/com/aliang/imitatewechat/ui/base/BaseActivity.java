45f4cc414eab0fa953c73f16383b37294a210fd7
MR-ALIiang <18629411463@163.com>
2018-04-26 17:19:46 +0800

    1:第一次提交代码,测试提交是否成功

---
+++ app/src/main/java/com/aliang/imitatewechat/ui/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.aliang.imitatewechat.ui.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by AlvinMoon on 2018/4/26.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+}
